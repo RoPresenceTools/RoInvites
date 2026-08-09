a4822fded8fddb8c2fbcf65544cf901ff129e0ac
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-08-09 15:24:37 -0400

    fix: prevent bot from crashing when sending an invite

--- cogs/user_cog.py
+++ cogs/user_cog.py
@@ -126,8 +126,8 @@ class UserCog(commands.Cog):
         if thumbnail_url is not None:
             embed.set_thumbnail(url=thumbnail_url)
 
+        view = discord.ui.View()
         if join_url is not None:
-            view = discord.ui.View()
             join_btn = discord.ui.Button(label="Join in Roblox", url=join_url)
             view.add_item(join_btn)
 
