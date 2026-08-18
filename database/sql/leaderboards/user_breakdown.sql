f0b5f100f730989e897d7c6a10b4ed475a1f3ee7
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-08-18 11:44:07 -0400

    refactor: organize leaderboard code by command group instead of by function

--- database/sql/leaderboards/user_breakdown.sql
+++ database/sql/leaderboards/user_breakdown.sql
@@ -1,3 +1,47 @@
+-- query: get_entries_total_playtimes
+SELECT COUNT(*)
+FROM (
+    SELECT
+        t.user_id,
+        COALESCE(t.total_playtime, 0)
+        + COALESCE(EXTRACT(EPOCH FROM (NOW() - cp.start_time)), 0) AS total_playtime
+    FROM total_playtimes t
+    LEFT JOIN currently_playing cp
+        ON cp.user_id = t.user_id
+    WHERE t.user_id = ANY($1)
+) playtimes;
+
+-- query: get_total_total_playtimes
+SELECT SUM(total_playtime)
+FROM (
+    SELECT
+        t.user_id,
+        COALESCE(t.total_playtime, 0)
+        + COALESCE(EXTRACT(EPOCH FROM (NOW() - cp.start_time)), 0) AS total_playtime
+    FROM total_playtimes t
+    LEFT JOIN currently_playing cp
+        ON cp.user_id = t.user_id
+    WHERE t.user_id = ANY($1)
+) playtimes;
+
+-- query: get_total_playtimes
+SELECT
+    user_id,
+    total_playtime,
+    RANK() OVER (ORDER BY total_playtime DESC) AS rank
+FROM (
+    SELECT
+        t.user_id,
+        COALESCE(t.total_playtime, 0)
+        + COALESCE(EXTRACT(EPOCH FROM (NOW() - cp.start_time)), 0) AS total_playtime
+    FROM total_playtimes t
+    LEFT JOIN currently_playing cp
+        ON cp.user_id = t.user_id
+    WHERE t.user_id = ANY($1)
+) playtimes
+LIMIT 10
+OFFSET $2;
+
 -- query: get_entries_ls_total_playtimes
 SELECT COUNT(*)
 FROM (
