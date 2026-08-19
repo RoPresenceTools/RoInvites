ffa319f69816d52d8630aa4d5a17fba49a8cbc2a
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-08-19 12:03:22 -0400

    docs(changelog): add the "a row for guild settings will be created on bot join" change

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ patch_notes = """
 Updated from __v{0}__ to __v{1}__
 
 **Patch Notes:**
+- If the bot joins a new guild, a row for guild settings will now be properly initalized
 - Leaderboard functions have been moved to their own .sql files
 - Leaderboards will still show after they time out
 - Rebranded the bot to RoInvites to prevent copyright issues
