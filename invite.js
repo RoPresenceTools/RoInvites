cc45e1e92a2ba104f67a256ef9611532d0788cf5
Aaron Wijesinghe <181608531+aaronwijes@users.noreply.github.com>
2026-08-30 22:45:41 -0500

    feat: valid the gameInstanceId parameter if it is given

--- invite.js
+++ invite.js
@@ -2,8 +2,13 @@ let params = new URLSearchParams(document.location.search);
 let placeId = params.get("placeId");
 let gameInstanceId = params.get("gameInstanceId");
 
+function hasNoSpaces(str) {
+    if (str.trim() === "") return false;
+    return str.replace(/\s/g, "") === str; 
+}
+
 function isPosNumber(str) {
-    if (typeof str !== 'string' || str.trim() === '') return false;
+    if (str.trim() === "") return false;
     return (!isNaN(Number(str)) && Number(str) > 0);
 }
 
@@ -14,6 +19,8 @@ function join() {
         alert("A valid placeId must be given as a URL parameter.");
     } else if (gameInstanceId == undefined) {
         location.href = "roblox://experiences/start?placeId=" + placeId;
+    } else if (!hasNoSpaces(gameInstanceId)) {
+        alert("A valid gameInstanceId must optionally be given as a URL parameter.");
     } else {
         location.href = "roblox://experiences/start?placeId=" + placeId + "&gameInstanceId=" + gameInstanceId;
     }
