3ae4f7bb7df0547a6563023aa5d78ad8a0e1a977
Aaron Wijesinghe <181608531+aaronwijes@users.noreply.github.com>
2026-08-30 22:10:49 -0500

    feat: show an error if no place ID is present in the URL

--- invite.js
+++ invite.js
@@ -4,11 +4,13 @@ let gameInstanceId = params.get("gameInstanceId");
 
 function join() {
     if (placeId == undefined && gameInstanceId == undefined) {
-        alert("A valid placeId and/or gameInstanceId must be given as URL parameters.")
+        alert("A valid placeId and/or gameInstanceId must be given as URL parameters.");
+    } else if (placeId == undefined) {
+        alert("A valid placeId must be given as a URL parameter.");
     } else if (gameInstanceId == undefined) {
-        location.href = "roblox://experiences/start?placeId=" + placeId
+        location.href = "roblox://experiences/start?placeId=" + placeId;
     } else {
-        location.href = "roblox://experiences/start?placeId=" + placeId + "&gameInstanceId=" + gameInstanceId
+        location.href = "roblox://experiences/start?placeId=" + placeId + "&gameInstanceId=" + gameInstanceId;
     }
 }
 
