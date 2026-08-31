7641a112dfcaf1997084a852fa008da0318142fe
Aaron Wijesinghe <181608531+aaronwijes@users.noreply.github.com>
2026-08-31 09:08:12 -0500

    feat: move setting element values to a different function

--- invite.js
+++ invite.js
@@ -12,23 +12,33 @@ function isPosNumber(str) {
     return (!isNaN(Number(str)) && Number(str) > 0);
 }
 
+function setParagraphElementValues() {
+    if (placeId == undefined && gameInstanceId == undefined) {
+        return;
+    } else if (isPosNumber(placeId) && gameInstanceId == undefined) {
+        document.getElementById("placeId").textContent = "Place ID: " + placeId;
+    } else if (hasNoSpaces(gameInstanceId)) {
+        document.getElementById("placeId").textContent = "Place ID: " + placeId;
+        document.getElementById("gameInstanceId").textContent = "Game Instance ID: " + gameInstanceId;
+    }
+}
+
 function join() {
     if (placeId == undefined && gameInstanceId == undefined) {
         alert("A valid placeId and gameInstanceId must be given as URL parameters.");
     } else if (placeId == undefined || !isPosNumber(placeId)) {
         alert("A valid placeId must be given as a URL parameter.");
     } else if (gameInstanceId == undefined) {
-        document.getElementById("placeId").textContent = "Place ID: " + placeId;
         location.href = "roblox://experiences/start?placeId=" + placeId;
     } else if (!hasNoSpaces(gameInstanceId)) {
         alert("A valid gameInstanceId must optionally be given as a URL parameter.");
     } else {
-        document.getElementById("placeId").textContent = "Place ID: " + placeId;
-        document.getElementById("gameInstanceId").textContent = "Game Instance ID: " + gameInstanceId;
         location.href = "roblox://experiences/start?placeId=" + placeId + "&gameInstanceId=" + gameInstanceId;
     }
 }
+
 window.onload = function(){
+    setParagraphElementValues();
     requestAnimationFrame(() => {
         setTimeout(function(){join()}, 0);
     });
