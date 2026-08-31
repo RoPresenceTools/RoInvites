adee01070df9eeacd8324b1daefca710628cab81
Aaron Wijesinghe <181608531+aaronwijes@users.noreply.github.com>
2026-08-31 10:23:43 -0500

    fix: if placeId is an empty string, don't allow it

--- invite.js
+++ invite.js
@@ -19,8 +19,8 @@ function setParagraphElementValues() {
         document.getElementById("placeId").textContent = "Place ID: Invalid";
     } else if (isPosNumber(placeId) && gameInstanceId == undefined) {
         document.getElementById("placeId").textContent = "Place ID: " + placeId;
-    } else if (gameInstanceId === "") {
-        document.getElementById("gameInstanceId").textContent = "Game Instance ID: Not Provided"
+    } else if (gameInstanceId.trim() === "") {
+        document.getElementById("gameInstanceId").textContent = "Game Instance ID: Not Provided";
     } else if (!hasNoSpaces(gameInstanceId)) {
         document.getElementById("gameInstanceId").textContent = "Game Instance ID: Invalid";
     } else {
@@ -30,7 +30,7 @@ function setParagraphElementValues() {
 }
 
 function join() {
-    if (placeId == undefined && gameInstanceId == undefined) {
+    if ((placeId == undefined && gameInstanceId == undefined) || !hasNoSpaces(placeId)) {
         return;
     } else if (gameInstanceId == undefined && isPosNumber(placeId)) {
         location.href = "roblox://experiences/start?placeId=" + placeId;
