1bfe5adbc5f1c6e01eed11d2d9f0be136bbc8887
Aaron Wijesinghe <aaronw1975@icloud.com>
2026-09-02 21:50:50 -0400

    feat: add user configuration through /user config

--- cogs/server_cog.py
+++ cogs/server_cog.py
@@ -3,6 +3,186 @@ from discord import app_commands
 from discord.ext import commands
 from styling.ri_colors import *
 
+class BackButton(discord.ui.Button):
+    def __init__(self):
+        super().__init__()
+        self.label = "Go back"
+        self.style = discord.ButtonStyle.secondary
+        self.callback = self.back_button_callback
+
+    async def back_button_callback(self, interaction: discord.Interaction):
+        main_menu_view = SetupView()
+        main_menu_embed = await main_menu_view.get_embed()
+        await interaction.response.edit_message(
+            embed=main_menu_embed,
+            view=main_menu_view
+        )
+
+class SuccessView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.go_back = BackButton()
+        self.add_item(self.go_back)
+
+    async def on_timeout(self):
+        self.go_back.disabled = True
+        self.stop()
+
+    async def get_embed(self, description):
+        embed = discord.Embed(
+            title="Success",
+            description=description,
+            color=green
+        )
+        return embed
+
+class FailureView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.go_back = BackButton()
+        self.add_item(self.go_back)
+
+    async def on_timeout(self):
+        self.go_back.disabled = True
+        self.stop()
+
+    async def get_embed(self, description):
+        embed = discord.Embed(
+            title="Failed",
+            description=description,
+            color=red
+        )
+        return embed
+
+class ChannelView(discord.ui.View):
+    def __init__(self, channel_type):
+        super().__init__()
+        self.result = None
+        self.go_back = BackButton()
+        self.channel_type = channel_type
+        self.new_channel_name = "roinvites" if self.channel_type == "invite" else "announcements"
+
+        self.channel_select = discord.ui.ChannelSelect(
+            placeholder="Choose a channel...",
+        )
+        async def select_callback(interaction: discord.Interaction):
+            self.result = self.channel_select.values[0]
+            await interaction.response.defer()
+            self.stop()
+
+        self.create_channel_button = discord.ui.Button(
+            label="Create Channel",
+            style=discord.ButtonStyle.primary
+        )
+
+        async def button_callback(interaction: discord.Interaction):
+            if not interaction.guild.me.guild_permissions.manage_channels:
+                failure_view = FailureView()
+                await interaction.response.edit_message(
+                    embed=await failure_view.get_embed("A server admin must enable the **Manage Channels** permission to create new channels."),
+                    view=failure_view
+                )
+
+            channel = await interaction.guild.create_text_channel(self.new_channel_name)
+            success = await interaction.client.settings_manager.set_channel(interaction.guild, self.channel_type, channel)
+            if success == True:
+                success_view = SuccessView()
+                await interaction.response.edit_message(
+                    embed = await success_view.get_embed(f"Set the {self.channel_type} channel to https://discord.com/channels/{interaction.guild.id}/{channel.id}"),
+                    view=success_view
+                )
+            else:
+                failure_view = FailureView()
+                await interaction.edit_original_response(
+                    embed=await failure_view.get_embed(f"Channel ID `{channel.id}` doesn't exist."),
+                    view=failure_view
+                )
+
+        self.channel_select.callback = select_callback
+        self.create_channel_button.callback = button_callback
+        self.add_item(self.channel_select)
+        self.add_item(self.create_channel_button)
+        self.add_item(self.go_back)
+
+    async def on_timeout(self):
+        self.result = None
+        self.create_channel_button.disabled = True
+        self.go_back.disabled = True
+        self.stop()
+
+    async def get_embed(self, description):
+        embed = discord.Embed(
+            title="RoInvites Setup",
+            description=description,
+            color=discord.Color.dark_gold()
+        )
+        return embed
+
+class SetupView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+    @discord.ui.select(
+        placeholder="Choose an option...",
+        options=[
+            discord.SelectOption(label="Invite Channel", value="invite", emoji="👥", description="This channel is where invite/leave messages are sent."),
+            discord.SelectOption(label="Announcement Channel", value="announcement", emoji="📢", description="This channel is where global announcements are sent.")
+        ]
+    )
+    async def callback(self, interaction: discord.Interaction, select):
+        if select.values[0] in ["invite", "announcement"]:
+            channel_view = ChannelView(select.values[0])
+            await interaction.response.edit_message(
+                embed=await channel_view.get_embed(f"Set the {select.values[0]} channel:"),
+                view=channel_view
+            )
+            await channel_view.wait()
+
+            result = channel_view.result
+            if result is None:
+                return
+            channel = result.resolve()
+            permissions = channel.permissions_for(interaction.guild.me)
+            if not (
+                permissions.view_channel and
+                permissions.send_messages and
+                permissions.embed_links
+            ):
+                failure_view = FailureView()
+                await interaction.edit_original_response(
+                    embed=await failure_view.get_embed(f"I don't have the necessary permissions to operate in https://discord.com/channels/{interaction.guild.id}/{channel.id}.\nEnable the **View Channel**, **Send Messages**, and **Embed Links** permissions for me in that channel."),
+                    view=failure_view
+                )
+                return
+
+            success = await interaction.client.settings_manager.set_channel(interaction.guild, select.values[0], channel)
+            if success:
+                success_view = SuccessView()
+                await interaction.edit_original_response(
+                    embed=await success_view.get_embed(f"Set the {select.values[0]} channel to https://discord.com/channels/{interaction.guild.id}/{channel.id}"),
+                    view=success_view
+                )
+            else:
+                failure_view = FailureView()
+                await interaction.edit_original_response(
+                    embed=await failure_view.get_embed(f"Channel ID `{channel.id}` doesn't exist."),
+                    view=failure_view
+                )
+
+    async def on_timeout(self):
+        self.stop()
+
+    async def get_embed(self):
+        embed = discord.Embed(
+            title="RoInvites Setup",
+            description="""
+            Set an option to configure!
+            You can configure the bot's channels, with much more being configurable soon.
+            """,
+            color=discord.Color.dark_gold()
+        )
+        return embed
+
 class ServerCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
@@ -32,6 +212,17 @@ class ServerCog(commands.Cog):
             private_channel=False
         )
     )
+
+    @server.command(name="config", description="Configure RoInvites in this server")
+    @app_commands.default_permissions(manage_guild=True)
+    @app_commands.checks.has_permissions(manage_guild=True)
+    async def config(
+        self, 
+        interaction: discord.Interaction
+    ):
+        view = SetupView()
+        await interaction.response.send_message(embed=await view.get_embed(), view=view, ephemeral=True)
+        await view.wait()
 
     @server.command(name="link", description="Links your account with the current server")
     async def link(
--- cogs/user_cog.py
+++ cogs/user_cog.py
@@ -3,6 +3,175 @@ from discord import app_commands
 from discord.ext import commands
 from styling.ri_colors import *
 
+class BackButton(discord.ui.Button):
+    def __init__(self):
+        super().__init__()
+        self.label = "Go back"
+        self.style = discord.ButtonStyle.secondary
+        self.callback = self.back_button_callback
+
+    async def back_button_callback(self, interaction: discord.Interaction):
+        main_menu_view = MainMenuView()
+        main_menu_embed = await main_menu_view.get_embed()
+        await interaction.response.edit_message(
+            embed=main_menu_embed,
+            view=main_menu_view
+        )
+
+class SuccessView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.go_back = BackButton()
+        self.add_item(self.go_back)
+
+    async def on_timeout(self):
+        self.go_back.disabled = True
+        self.stop()
+
+    async def get_embed(self, description):
+        embed = discord.Embed(
+            title="Success",
+            description=description,
+            color=green
+        )
+        return embed
+
+class FailureView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.go_back = BackButton()
+        self.add_item(self.go_back)
+
+    async def on_timeout(self):
+        self.go_back.disabled = True
+        self.stop()
+
+    async def get_embed(self, description):
+        embed = discord.Embed(
+            title="Failed",
+            description=description,
+            color=red
+        )
+        return embed
+
+class UsernameModal(discord.ui.Modal):
+    def __init__(self):
+        super().__init__(title="Roblox Username")
+        self.value = None
+        self.username_input = discord.ui.TextInput(
+            label="Username",
+            placeholder="Enter your Roblox username..."
+        )
+        self.add_item(self.username_input)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        self.value = self.username_input.value
+        await interaction.response.defer()
+
+    async def on_timeout(self):
+        self.stop()
+
+class MainMenuView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+    @discord.ui.select(
+        placeholder="Choose an option...",
+        options=[
+            discord.SelectOption(label="Add Account", value="add", emoji="➕", description="Add your Roblox account to RoInvites"),
+            discord.SelectOption(label="Remove Account", value="remove", emoji="❌", description="Remove your Roblox account from RoInvites"),
+            discord.SelectOption(label="Freeze Account", value="freeze", emoji="❄️", description="Freeze your RoInvites account"),
+            discord.SelectOption(label="Unfreeze Account", value="unfreeze", emoji="🔥", description="Unfreeze your RoInvites account")
+        ]
+    )
+    async def callback(self, interaction: discord.Interaction, select):
+        if select.values[0] == "add":
+            username_modal = UsernameModal()
+            await interaction.response.send_modal(username_modal)
+            await username_modal.wait()
+
+            username = username_modal.value
+            if username.strip() == "":
+                failure_view = FailureView()
+                await interaction.edit_original_response(
+                    embed=await failure_view.get_embed("No username was given."),
+                    view=failure_view
+                )
+                self.stop()
+
+            success = await interaction.client.user_manager.add_user(username, interaction.user)
+            if success == True:
+                success_view = SuccessView()
+                await interaction.edit_original_response(
+                    embed=await success_view.get_embed(f"Successfully added you (@{username}) to RoInvites!\nRun `/server link` in any server with me in it to participate in leaderboards and events!"),
+                    view=success_view
+                )
+            else:
+                failure_view = FailureView()
+                await interaction.edit_original_response(
+                    embed=await failure_view.get_embed(success),
+                    view=failure_view
+                )
+        elif select.values[0] == "remove":
+            success = await interaction.client.user_manager.remove_user(interaction.user)
+            if success == True:
+                success_view = SuccessView()
+                await interaction.response.edit_message(
+                    embed=await success_view.get_embed(f"Successfully removed you from RoInvites.\nHope you had a great time!"),
+                    view=success_view
+                )
+            else:
+                failure_view = FailureView()
+                await interaction.response.edit_message(
+                    embed = await failure_view.get_embed(f"You don't have a Roblox account associated with RoInvites.\nAdd one with `/user add`!"),
+                    view=failure_view
+                )
+        elif select.values[0] == "freeze":
+            success = await interaction.client.user_manager.modify_freeze_user(interaction.user, 1)
+            if success == True:
+                success_view = SuccessView()
+                await interaction.response.edit_message(
+                    embed=await success_view.get_embed(f"Successfully froze your account!"),
+                    view=success_view
+                )
+            else:
+                failure_view = FailureView()
+                await interaction.response.edit_message(
+                    embed=await failure_view.get_embed(f"You don't have a Roblox account associated with RoInvites.\nAdd one with `/user add`!"),
+                    view=failure_view
+                )
+        elif select.values[0] == "unfreeze":
+            success = await interaction.client.user_manager.modify_freeze_user(interaction.user, 0)
+            if success == True:
+                success_view = SuccessView()
+                await interaction.response.edit_message(
+                    embed=await success_view.get_embed(f"Successfully unfroze your account!"),
+                    view=success_view
+                )
+            else:
+                failure_view = FailureView()
+                await interaction.response.edit_message(
+                    embed=await failure_view.get_embed(f"You don't have a Roblox account associated with RoInvites.\nAdd one with `/user add`!"),
+                    view=failure_view
+                )
+
+    async def on_timeout(self):
+        self.stop()
+
+    async def get_embed(self):
+        embed = discord.Embed(
+            title="RoInvites Account Menu",
+            description="""            
+            **Select an option below:**
+            1. Add your Roblox account
+            2. Remove your account
+            3. Freeze your account
+            4. Unfreeze your account
+            """,
+            color=discord.Color.dark_gold()
+        )
+        return embed
+
 class UserCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
@@ -20,6 +189,15 @@ class UserCog(commands.Cog):
             private_channel=True
         )
     )
+
+    @user.command(name="config", description="Configure your RoInvites account")
+    async def add_user(
+        self, 
+        interaction: discord.Interaction
+    ):
+        view = MainMenuView()
+        await interaction.response.send_message(embed=await view.get_embed(), view=view, ephemeral=True)
+        await view.wait()
 
     @user.command(name="add", description="Adds a new user to RoInvites")
     async def add_user(
